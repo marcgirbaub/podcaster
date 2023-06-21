6f49c03417a434ccb0c16b5a59a989757075654e
Marc Girbau <marc.girbau94@gmail.com>
2023-06-21 20:06:49 +0200

    Create Home page component

--- src/router/router.tsx
+++ src/router/router.tsx
@@ -1,6 +1,9 @@
 import { RouteObject, createBrowserRouter } from "react-router-dom";
 import App from "../components/App/App";
+import Home from "../pages/Home/Home";
 
-const routes: RouteObject[] = [{ path: "/", element: <App />, children: [{}] }];
+const routes: RouteObject[] = [
+  { element: <App />, children: [{ path: "/", element: <Home /> }] },
+];
 
 export const router = createBrowserRouter(routes);
