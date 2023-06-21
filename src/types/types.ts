e83e17e9eff061448946548b23229ebe26db22f5
Marc Girbau <marc.girbau94@gmail.com>
2023-06-21 15:00:21 +0200

    Change api response types

--- src/types/types.ts
+++ src/types/types.ts
@@ -1,35 +1,46 @@
+export interface Image {
+  label: string;
+  attributes: { height: string };
+}
+
+export interface Podcast {
+  "im:name": { label: string };
+  "im:image": Image[];
+  summary: { label: string };
+  "im:price": {
+    label: string;
+    attributes: { amount: string; currency: string };
+  };
+  "im:contentType": { attributes: { term: string; label: string } };
+  rights: { label: string };
+  title: { label: string };
+  link: { attributes: { rel: string; type: string; href: string } };
+  id: { label: string; attributes: { "im:id": string } };
+  "im:artist": { label: string; attributes?: { href: string } };
+  category: {
+    attributes: {
+      "im:id": string;
+      term: string;
+      scheme: string;
+      label: string;
+    };
+  };
+  "im:releaseDate": { label: string; attributes: { label: string } };
+}
+
+export interface Link {
+  attributes: { rel: string; type?: string; href: string };
+}
+
 export interface TopPodcastsResponse {
   feed: {
     author: { name: { label: string }; uri: { label: string } };
-    entry: {
-      "im:name": { label: string };
-      "im:image": [{ label: string; attributes: { height: string } }];
-      summary: { label: string };
-      "im:price": {
-        label: string;
-        attributes: { amount: string; currency: string };
-      };
-      "im:contentType": { attributes: { term: string; label: string } };
-      rights: { label: string };
-      title: { label: string };
-      link: { attributes: { rel: string; type: string; href: string } };
-      id: { label: string; attributes: { "im:id": string } };
-      "im:artist": { label: string; attributes: { href: string } };
-      category: {
-        attributes: {
-          "im:id": string;
-          term: string;
-          scheme: string;
-          label: string;
-        };
-      };
-      "im:releaseDate": { label: string; attributes: { label: string } };
-    };
+    entry: Podcast[];
     updated: { label: string };
     rights: { label: string };
     title: { label: string };
     icon: { label: string };
-    link: [{ attributes: { rel: string; type: string; href: string } }];
+    link: Link[];
     id: { label: string };
   };
 }
