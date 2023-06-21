d3736895ba334aef8398c7d71399a8d07a5a1a76
Marc Girbau <marc.girbau94@gmail.com>
2023-06-21 23:13:03 +0200

    Create Layout component

--- src/components/App/App.tsx
+++ src/components/App/App.tsx
@@ -1,15 +1,13 @@
 import { QueryClient, QueryClientProvider } from "react-query";
-import { Outlet } from "react-router-dom";
+import Layout from "../Layout/Layout";
 
 const App = (): JSX.Element => {
   const queryClient = new QueryClient();
 
   return (
-    <>
-      <QueryClientProvider client={queryClient}>
-        <Outlet />
-      </QueryClientProvider>
-    </>
+    <QueryClientProvider client={queryClient}>
+      <Layout />
+    </QueryClientProvider>
   );
 };
 
--- src/components/Header/HeaderStyled.ts
+++ src/components/Header/HeaderStyled.ts
@@ -3,9 +3,12 @@ import styled from "@emotion/styled";
 const HeaderStyled = styled.header`
   width: 100vw;
   position: fixed;
+  top: 0;
+  z-index: 2;
   height: 54px;
   padding: 14px;
   box-shadow: 0px 2px 2px rgba(0, 0, 0, 0.1);
+  background-color: #fff;
 
   .header {
     &__title {
