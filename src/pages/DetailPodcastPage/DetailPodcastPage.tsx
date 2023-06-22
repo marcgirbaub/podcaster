aaee086eae43680ea9d1bc4fdf7621433b9006af
Marc Girbau <marc.girbau94@gmail.com>
2023-06-22 18:03:31 +0200

    Test PodcastProfile component

--- src/pages/DetailPodcastPage/DetailPodcastPage.tsx
+++ src/pages/DetailPodcastPage/DetailPodcastPage.tsx
@@ -4,11 +4,7 @@ import { useAppSelector } from "../../store";
 const DetailPodcastPage = (): JSX.Element => {
   const podcast = useAppSelector((state) => state.podcasts);
 
-  return (
-    <>
-      <PodcastProfile podcast={podcast} />
-    </>
-  );
+  return <PodcastProfile podcast={podcast} />;
 };
 
 export default DetailPodcastPage;
