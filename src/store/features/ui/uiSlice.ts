dbb08d1b8d62709e10c25c37d781434340535c82
Marc Girbau <marc.girbau94@gmail.com>
2023-06-22 11:50:22 +0200

    Create setIsLoading and unsetIsLoading reducer cases

--- src/store/features/ui/uiSlice.ts
+++ src/store/features/ui/uiSlice.ts
@@ -6,7 +6,20 @@ export const initialUiState: UiState = { isLoading: false };
 const uiSlice = createSlice({
   name: "ui",
   initialState: initialUiState,
-  reducers: {},
+  reducers: {
+    setIsLoading: (currentState): UiState => ({
+      ...currentState,
+      isLoading: true,
+    }),
+    unsetIsLoading: (currentState): UiState => ({
+      ...currentState,
+      isLoading: false,
+    }),
+  },
 });
 
 export const uiReducer = uiSlice.reducer;
+export const {
+  setIsLoading: setIsLoadingActionCreator,
+  unsetIsLoading: unsetIsLoadingActionCreator,
+} = uiSlice.actions;
