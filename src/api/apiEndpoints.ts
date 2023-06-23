0a64217776130ce54e3d562c3c5ad90b7122d7a6
Marc Girbau <marc.girbau94@gmail.com>
2023-06-23 17:13:20 +0200

    Fix endpoint to detail podcast

--- src/api/apiEndpoints.ts
+++ src/api/apiEndpoints.ts
@@ -1,7 +1,6 @@
 export const baseUrl = "https://itunes.apple.com/";
 export const podcastsListEndpoint = "us/rss/toppodcasts/";
-export const podcastEpisodesEndpoint =
-  "https://cors-anywhere.herokuapp.com/https://itunes.apple.com/lookup?";
+export const podcastEpisodesEndpoint = "https://itunes.apple.com/lookup?";
 export const limitParam = "limit";
 export const genreParam = "genre";
 
