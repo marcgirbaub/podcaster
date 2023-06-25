fc2ce0065b7a800b005d8290b212732df9d17775
Marc Girbau <marc.girbau94@gmail.com>
2023-06-25 21:19:49 +0200

    Include fix to cors error

--- src/api/apiEndpoints.ts
+++ src/api/apiEndpoints.ts
@@ -1,6 +1,8 @@
-export const baseUrl = "https://itunes.apple.com/";
+export const baseUrl =
+  "https://cors-anywhere.herokuapp.com/https://itunes.apple.com/";
 export const podcastsListEndpoint = "us/rss/toppodcasts/";
-export const podcastEpisodesEndpoint = "https://itunes.apple.com/lookup?";
+export const podcastEpisodesEndpoint =
+  "https://cors-anywhere.herokuapp.com/https://itunes.apple.com/lookup?";
 export const limitParam = "limit";
 export const genreParam = "genre";
 export const mediaParam = "podcast";
